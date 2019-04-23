db85beb7c7d5b1c371908063dba9bf4a65df4de6
Kevin Emmrich <kemmrich@jkt9000.com>
2019-04-23 07:31:33 -0400

    Initial creation of projects required for MyShop
    MyShop.* projects

---
+++ MyShop.Core/Models/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        private string _id;
+        private string _name;
+        private string _description;
+        private decimal _price;
+        private string _category;
+        private string _image;
+
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        [StringLength(20)]
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        [Range(0,1000)]
+        [DisplayName("Product Name")]
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+
+        public string Image
+        {
+            get { return _image; }
+            set { _image = value; }
+        }
+
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+
+    }
+}
---
+++ MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products; 
+
+
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => product.Id == product.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product to be updated Not Found!");
+            }
+        }
+
+
+
+        public Product Find(string id)
+        {
+            Product productToFind = products.Find(p => p.Id == id);
+            if (productToFind != null)
+            {
+                return productToFind;
+            }
+            else
+            {
+                throw new Exception($"Cannot find product with ID of {id}!");
+            }
+
+        }
+
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string id)
+        {
+            Product productToDelete = products.Find(p => p.Id == id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception($"Cannot find product with ID of {id} to delete!");
+            }
+        }
+
+
+    } // end class ProductRepository
+} // end namespace MyShop.DataAccess.InMemory
