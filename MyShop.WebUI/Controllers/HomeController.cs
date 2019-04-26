20f46aed457d22c9c8e24e1376f6200e4c84cab8
Kevin Emmrich <kemmrich@jkt9000.com>
2019-04-26 15:55:22 -0400

    adding product filter list-group

--- MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +22,26 @@ namespace MyShop.WebUI.Controllers
         }
 
 
-        public ActionResult Index()
+        public ActionResult Index(string category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products; // = context.Collection().ToList();
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (category == null)
+            {
+                // show all products
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string id)
